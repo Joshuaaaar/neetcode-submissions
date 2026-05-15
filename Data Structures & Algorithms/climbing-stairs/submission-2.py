00a1c44beb3904a3744634e4394e9faff8b45aa3
Joshua Ramnauth <joshua20050710@gmail.com>
2026-05-14 20:35:24 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        memo = {1:1,2:2}
+
+        def fib(n):
+
+            if n in memo:
+                return memo[n]
+            
+            else:
+                memo[n] = fib(n-1) + fib(n-2)
+                return memo[n]
+
+        return fib(n)
+
